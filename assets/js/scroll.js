8249f270cf2ef51b492ffa227c3dc3a88d68654d
MrDynimate <1812909518@qq.com>
2025-11-24 21:06:41 +0800

    feat(nav): stop flow & add scroll to hero

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,3 +1,7 @@
 function scrollToShowcase() {
     document.getElementById('showcase').scrollIntoView({ behavior: 'smooth' });
 }
+
+function scrollToHeropage() {
+    document.getElementById('hero').scrollIntoView({ behavior: 'smooth' });
+}
